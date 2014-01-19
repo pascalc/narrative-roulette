69d1f91c48ab022a56232debdec14f5a5a449cbc
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-19 14:02:09 +0100

    Use flask redirects for random perspective and latest round

--- src/handlers/custom.py
+++ src/handlers/custom.py
@@ -9,6 +9,10 @@ def index():
 
 @app.route('/api/perspective/random')
 def random_perspective():
-  return flask.jsonify(
-    q.random_perspective().to_dict()
-  )
+  random_id = q.random_perspective().id
+  return flask.redirect('/api/perspective/%s' % random_id)
+
+@app.route('/api/round/latest')
+def latest_round():
+  round_id = q.latest_round().id
+  return flask.redirect("/api/round/%s" % round_id)
