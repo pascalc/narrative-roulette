a489770585afa75576018b9acc103816be55ef26
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-19 14:49:15 +0100

    Use moment.js to display how long a round has been running for

--- src/handlers/static/js/DisplayCtrl.js
+++ src/handlers/static/js/DisplayCtrl.js
@@ -1,3 +1,24 @@
-function DisplayCtrl() {
-  
+var getLatestRound,
+    updateElapsedTime;
+function DisplayCtrl($scope, $http, $interval) {
+  $scope.latest_round;
+  $scope.get_latest_round = function() {
+    return $http.get("/api/round/latest").then(function(response) {
+      $scope.latest_round = response.data;
+    });
+  }
+
+  $scope.elapsed_time;
+  $scope.update_elapsed_time = function() {
+    $scope.elapsed_time = moment($scope.latest_round.start_time).fromNow();
+  }
+
+  angular.element(document).ready(function () {
+    $scope.get_latest_round().then(function() {
+      $interval($scope.update_elapsed_time, 1000);
+    });
+  });
+
+  getLatestRound = $scope.get_latest_round;
+  updateElapsedTime = $scope.update_elapsed_time;
 }
