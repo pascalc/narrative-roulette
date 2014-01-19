24de510bd960afb961534738b6c2ccf00537ac77
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-19 15:56:23 +0100

    Added Round.submissions as a hybrid_property

--- src/db/query.py
+++ src/db/query.py
@@ -7,9 +7,6 @@ from sqlalchemy import desc
 import db
 from db.schema import *
 
-Session = orm.sessionmaker(bind=db.engine)
-session = orm.scoped_session(Session)
-
 def random_perspective():
   return session.query(Perspective)\
     .order_by(sql.func.rand())\
--- src/db/schema.py
+++ src/db/schema.py
@@ -6,11 +6,15 @@ import sqlalchemy as sql
 from sqlalchemy import Table, Column, Integer, String, DateTime, Text, ForeignKey
 from sqlalchemy import orm
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property
 
 import db
 
 Base = declarative_base()
 Base.metadata.bind = db.engine
+
+Session = orm.sessionmaker(bind=db.engine)
+session = orm.scoped_session(Session)
 
 class MyMixin(object):
   def to_dict(self):
@@ -32,15 +36,6 @@ class Perspective(Base, MyMixin):
   def __repr__(self):
     return "%s, %s" % (self.gender, self.text)
 
-class Round(Base, MyMixin):
-  __tablename__ = 'round'
-  id = Column(Integer, primary_key=True)
-  start_time = Column(DateTime, default=datetime.now)
-  perspective_id = Column(Integer, ForeignKey('perspective.id'), nullable=False)
-
-  def __repr__(self):
-    return "Round %s" % self.id
-
 class Submission(Base, MyMixin):
   __tablename__ = 'submission'
   id = Column(Integer, primary_key=True)
@@ -52,6 +47,22 @@ class Submission(Base, MyMixin):
 
   def __repr__(self):
     return "Submission %s" % self.id
+
+class Round(Base, MyMixin):
+  __tablename__ = 'round'
+  id = Column(Integer, primary_key=True)
+  start_time = Column(DateTime, default=datetime.now)
+  perspective_id = Column(Integer, ForeignKey('perspective.id'), nullable=False)
+
+  @hybrid_property
+  def submissions(self):
+    return [s.to_dict() for s in session.query(Submission)\
+      .filter(Submission.created_at >= self.start_time)\
+      .filter(Submission.perspective_id == self.perspective_id)\
+      .all()]
+
+  def __repr__(self):
+    return "Round %s" % self.id
 
 Base.metadata.create_all()
 logging.info("Table information loaded")
