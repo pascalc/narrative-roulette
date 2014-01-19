f1516826d92edfcf8b12c9e06234e4050b0312cb
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-19 18:59:36 +0100

    Added rounds

--- src/handlers/static/js/RoundCtrl.js
+++ src/handlers/static/js/RoundCtrl.js
@@ -1,3 +1,12 @@
-function RoundCtrl() {
-
+function RoundCtrl($scope, $http) {
+  $scope.rounds;
+  $scope.get_rounds = function() {
+    $http.get("/api/round?results_per_page=100").then(function(response) {
+      $scope.rounds = response.data.objects;
+      $scope.rounds.reverse();
+    });
+  }
+  angular.element(document).ready(function () {
+    $scope.get_rounds();
+  });
 }
