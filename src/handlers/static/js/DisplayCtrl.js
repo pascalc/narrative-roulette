03741e9317113f7bbe73200d07724fb78cad919b
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-19 15:17:46 +0100

    Added roulette spinner and creation of new rounds

--- src/handlers/static/js/DisplayCtrl.js
+++ src/handlers/static/js/DisplayCtrl.js
@@ -1,6 +1,9 @@
 var getLatestRound,
-    updateElapsedTime;
+    updateElapsedTime,
+    postNewRound;
 function DisplayCtrl($scope, $http, $interval) {
+  // Latest round
+
   $scope.latest_round;
   $scope.get_latest_round = function() {
     return $http.get("/api/round/latest").then(function(response) {
@@ -13,12 +16,28 @@ function DisplayCtrl($scope, $http, $interval) {
     $scope.elapsed_time = moment($scope.latest_round.start_time).fromNow();
   }
 
+  // New round
+  $scope.random_perspective;
+  $scope.get_random_perspective = function() {
+    $http.get("/api/perspective/random").then(function(response) {
+      $scope.random_perspective = response.data;
+    });
+  }
+
+  $scope.post_new_round = function() {
+    $http.post("/api/round", {
+      'perspective_id': $scope.random_perspective.id
+    }).then($scope.get_latest_round);
+  };
+
   angular.element(document).ready(function () {
     $scope.get_latest_round().then(function() {
       $interval($scope.update_elapsed_time, 1000);
     });
+    $scope.get_random_perspective();
   });
 
   getLatestRound = $scope.get_latest_round;
   updateElapsedTime = $scope.update_elapsed_time;
+  postNewRound = $scope.post_new_round;
 }
