8513ae71046bdb94098ceb0024c5b5c81de3e01d
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-19 14:01:53 +0100

    Add latest round query

--- src/db/query.py
+++ src/db/query.py
@@ -2,6 +2,7 @@ import logging
 
 import sqlalchemy.orm as orm
 import sqlalchemy.sql as sql
+from sqlalchemy import desc
 
 import db
 from db.schema import *
@@ -12,5 +13,11 @@ session = orm.scoped_session(Session)
 def random_perspective():
   return session.query(Perspective)\
     .order_by(sql.func.rand())\
+    .limit(1)\
+    .first()
+
+def latest_round():
+  return session.query(Round)\
+    .order_by(desc(Round.start_time))\
     .limit(1)\
     .first()
