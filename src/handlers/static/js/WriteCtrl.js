46a0e7f5fd23b11c69096b0abee41be04753b82c
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-19 15:27:54 +0100

    Show round information on write partial

--- src/handlers/static/js/WriteCtrl.js
+++ src/handlers/static/js/WriteCtrl.js
@@ -1,7 +1,7 @@
 var getRandomPerspective,
     getPerspective,
     postSubmission;
-function WriteCtrl($scope, $routeParams, $http, $location) {
+function WriteCtrl($scope, $routeParams, $http, $interval) {
   $scope.perspective;
   $scope.submission;
 
@@ -18,15 +18,6 @@ function WriteCtrl($scope, $routeParams, $http, $location) {
     $http.get("/api/perspective/" + id).then(renderPerspective);
   }
 
-  angular.element(document).ready(function () {
-    var perspective_id = $routeParams.perspectiveId;
-    if (perspective_id) {
-      $scope.get_perspective(perspective_id);
-    } else {
-      $scope.get_random_perspective()  
-    }
-  });
-
   $scope.post_submission = function(submission) {
     submission.perspective_id = $scope.perspective.id;
     console.log("Submission:", submission);
@@ -34,6 +25,30 @@ function WriteCtrl($scope, $routeParams, $http, $location) {
       console.log(response);
     });
   }
+
+  $scope.latest_round;
+  $scope.get_latest_round = function() {
+    return $http.get("/api/round/latest").then(function(response) {
+      $scope.latest_round = response.data;
+    });
+  }
+
+  $scope.elapsed_time;
+  $scope.update_elapsed_time = function() {
+    $scope.elapsed_time = moment($scope.latest_round.start_time).fromNow();
+  }
+
+  angular.element(document).ready(function () {
+    var perspective_id = $routeParams.perspectiveId;
+    if (perspective_id) {
+      $scope.get_perspective(perspective_id);
+    } else {
+      $scope.get_latest_round().then(function(response) {      
+        $scope.get_perspective($scope.latest_round.perspective.id);
+        $interval($scope.update_elapsed_time, 1000);
+      });
+    }
+  });
 
   getRandomPerspective = $scope.get_random_perspective;
   getPerspective = $scope.get_perspective;
