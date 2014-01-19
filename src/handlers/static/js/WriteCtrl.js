3eb6e093ec6cd8695488bce35417cf7de5e8bfa2
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-19 12:08:32 +0100

    Allowed posting of submissions

--- src/handlers/static/js/WriteCtrl.js
+++ src/handlers/static/js/WriteCtrl.js
@@ -1,7 +1,9 @@
 var getRandomPerspective,
-    getPerspective;
-function WriteCtrl($scope, $routeParams, $http) {
+    getPerspective,
+    postSubmission;
+function WriteCtrl($scope, $routeParams, $http, $location) {
   $scope.perspective;
+  $scope.submission;
 
   function renderPerspective(response) {
     $scope.perspective = response.data;
@@ -25,6 +27,15 @@ function WriteCtrl($scope, $routeParams, $http) {
     }
   });
 
+  $scope.post_submission = function(submission) {
+    submission.perspective_id = $scope.perspective.id;
+    console.log("Submission:", submission);
+    $http.post("/api/submission", submission).then(function(response) {
+      console.log(response);
+    });
+  }
+
   getRandomPerspective = $scope.get_random_perspective;
   getPerspective = $scope.get_perspective;
+  postSubmission = $scope.post_submission;
 }
