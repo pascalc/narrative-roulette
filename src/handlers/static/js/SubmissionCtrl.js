cc1c8e76122c7060db294dec1d929ec94906c861
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-20 20:00:53 +0100

    Tried to add ng-keyup on submission viewer

--- src/handlers/static/js/SubmissionCtrl.js
+++ src/handlers/static/js/SubmissionCtrl.js
@@ -7,6 +7,15 @@ function SubmissionCtrl($scope, $routeParams, $http) {
     });
   }
 
+  $scope.key_pressed = function(event) {
+    console.log("Key pressed", event);
+    if (event.keyCode == 37 && $scope.show_submission_index != 0) {
+      $scope.show_submission_index = $scope.show_submission_index - 1;
+    } else if (event.keyCode == 39 && $scope.show_submission_index != $scope.round.submissions.length - 1) {
+      $scope.show_submission_index = $scope.show_submission_index + 1;
+    }
+  }
+
   angular.element(document).ready(function () {
     $scope.get_round($routeParams.roundId);
   });
