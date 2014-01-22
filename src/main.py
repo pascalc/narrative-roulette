680ca5c83a0c9b8c44871c8f381a3ac390eba8de
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-22 20:13:59 +0100

    Deployed on Google App Engine

--- src/main.py
+++ src/main.py
@@ -1,27 +1,27 @@
 # Flask
 from handlers.rest import app
-import handlers.admin
+# import handlers.admin
 import handlers.custom
 
 # Tornado WebSocket handler
-from handlers.socket import WSHandler
+# from handlers.socket import WSHandler
 
 # Tornado
-import tornado.wsgi
-import tornado.ioloop
-import tornado.web
-from tornado.log import enable_pretty_logging
+# import tornado.wsgi
+# import tornado.ioloop
+# import tornado.web
+# from tornado.log import enable_pretty_logging
 
-app.debug
+# app.debug
 # app.run(port=5000)
 
-container = tornado.wsgi.WSGIContainer(app)
-application = tornado.web.Application([
-  (r"/ws", WSHandler),
-  (r".*", tornado.web.FallbackHandler, dict(fallback=container)),
-], debug=True)
+# container = tornado.wsgi.WSGIContainer(app)
+# application = tornado.web.Application([
+#   (r"/ws", WSHandler),
+#   (r".*", tornado.web.FallbackHandler, dict(fallback=container)),
+# ], debug=True)
 
-if __name__ == "__main__":
-  application.listen(5000)
-  enable_pretty_logging()
-  tornado.ioloop.IOLoop.instance().start()
+# if __name__ == "__main__":
+#   application.listen(5000)
+#   enable_pretty_logging()
+#   tornado.ioloop.IOLoop.instance().start()
