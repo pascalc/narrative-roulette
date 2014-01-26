7f66bf79eae29ea2489051352dbc68ffef239113
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-26 17:11:35 +0100

    Styled submission view

--- src/handlers/static/js/SubmissionCtrl.js
+++ src/handlers/static/js/SubmissionCtrl.js
@@ -1,11 +1,18 @@
-function SubmissionCtrl($scope, $http, $routeParams) {
+function SubmissionCtrl($scope, $http, $routeParams, $timeout) {
   $scope.submission_id = $routeParams.submissionId;
   $scope.submission;
   $scope.get_submission = function(submission_id) {
     $http.get("/api/submission/" + submission_id).then(function(response) {
       $scope.submission = response.data;
+      $timeout(FB.XFBML.parse, 100);
       return response;
     });
   }
   $scope.get_submission($scope.submission_id);
+
+  angular.element(document).ready(function () {
+    if (typeof FB !== 'undefined') {
+      FB.XFBML.parse();  
+    }
+  });
 }
