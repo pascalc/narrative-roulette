9fbff98a62ea9e1200202f159905af180684a60f
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-29 19:25:15 +0100

    Added handler for rendering submission canonical URLs server-side

--- src/handlers/custom.py
+++ src/handlers/custom.py
@@ -1,10 +1,11 @@
 import logging
 
 import flask
-from flask import request, url_for
+from flask import request, url_for, render_template
 
 from handlers import app
 import db.query as q
+from db.schema import *
 
 def redirect(target, **kwargs):
   referer = request.headers.get("Referer")
@@ -28,3 +29,9 @@ def random_perspective():
 def latest_round():
   round_id = q.latest_round().id
   return redirect("/api/round/%s" % round_id)
+
+@app.route('/submission/<int:submission_id>')
+def serverside_submission(submission_id=None):
+  logging.info("Rendering submission server-side")
+  sub = session.query(Submission).filter(Submission.id == submission_id)[0]
+  return render_template("submission.html", submission=sub)
