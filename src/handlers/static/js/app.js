47c876250d552e32316808dbc11afac08ec63973
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-02-02 18:43:57 +0100

    Changed name of rounds link

--- src/handlers/static/js/app.js
+++ src/handlers/static/js/app.js
@@ -29,7 +29,7 @@ app.config(['$routeProvider',
         templateUrl: 'partials/submissionList.html',
         controller: 'SubmissionListCtrl'
       }).
-      when('/submissions', {
+      when('/rounds', {
         templateUrl: 'partials/submissions.html',
         controller: 'SubmissionsCtrl'
       }).
