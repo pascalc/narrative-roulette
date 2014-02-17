0b3ed86287d3610a54a412edc277d72b050354c5
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-02-17 09:57:53 +0100

    Added restart to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,11 @@
 from fabric.api import *
 
+path = '/home/ubuntu/kg-narrative-roulette'
+
 def deploy():
-  code_path = '/home/ubuntu/kg-narrative-roulette'
-  with cd(code_path):
+  with cd(path):
     run('git pull origin kg')
+
+def restart():
+  with cd(path):
+    run('sudo supervisorctl restart kg-narrative-roulette')
