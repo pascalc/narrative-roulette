a74716d482c43cdaf7dc38f4ba9524bc3d7d77da
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-02-19 00:28:01 +0100

    Refresh db.schema.session when 500 received

--- src/handlers/custom.py
+++ src/handlers/custom.py
@@ -5,6 +5,7 @@ from flask import request, url_for, render_template
 
 from handlers import app
 import db.query as q
+import db.schema
 from db.schema import *
 
 def redirect(target, **kwargs):
@@ -16,9 +17,6 @@ def redirect(target, **kwargs):
 
 @app.before_request
 def open_session():
-  if 'session' in flask.g:
-    logging.info("Closing previous session")
-    flask.g.session.close()
   logging.info("Opening session")
   flask.g.session = new_session()
 
@@ -27,6 +25,12 @@ def close_session(response):
   logging.info("Closing session")
   flask.g.session.close()
   return response
+
+def refresh_session(error):
+  logging.error(error)
+  db.schema.session = new_session()
+  flask.abort(500)
+app.error_handler_spec[None][500] = refresh_session
 
 @app.route('/')
 def index():
