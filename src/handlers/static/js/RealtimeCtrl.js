4fd75450fce375640e3d2cf99bd2dc49a5c2bb35
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-03-22 19:42:06 +0100

    Added start of realtime controller

---
+++ src/handlers/static/js/RealtimeCtrl.js
@@ -0,0 +1,31 @@
+var redis_prefix = "kg-narrative-roulette";
+
+function prefix(str) {
+  return redis_prefix + str;
+}
+
+var readers_key = "readers";
+var writers_key = "writers";
+function increment(key) {
+  return $.get("localhost:7379/INCR/" + prefix(key));
+}
+function decrement(key) {
+  return $.get("localhost:7379/DECR/" + prefix(key));
+}
+
+function publish(channel, obj) {
+ return $.get("localhost:7379/PUBLISH/" + prefix(channel) + "/" + JSON.stringify(obj)); 
+}
+function subscribe(channel) {
+  var socket = new WebSocket("ws://localhost:7379/.json");
+  socket.onopen = function() {
+      socket.send(JSON.stringify(["SUBSCRIBE", prefix(channel)]));
+  };
+  return socket;
+}
+
+function RealtimeCtrl($scope) {
+  $scope.$on('$locationChangeSuccess', function(event) {
+    console.log("$locationChangeSuccess", event);
+  });
+}
